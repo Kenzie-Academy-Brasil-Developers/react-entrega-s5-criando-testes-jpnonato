0adadc3525ccfe521d4547e277db7a3a114f05ca
jpnonato <jpnonatox@gmail.com>
2021-11-08 14:52:23 -0300

    task adjusts

--- src/__test__/app.test.tsx
+++ src/__test__/app.test.tsx
@@ -1,10 +1,12 @@
-import {screen, render, fireEvent} from '@testing-library/react'
+import {screen, render, fireEvent, waitFor} from '@testing-library/react'
 import userEvent from "@testing-library/user-event"
 import Search from '../components/Search'
 import Adress from '../components/Cep/index'
 import api from '../services'
+import App from '../App'
 import MockAdapter from 'axios-mock-adapter'
 import axios from 'axios'
+import Providers from '../providers'
 
 
 const apiMock = new MockAdapter(api)
@@ -16,6 +18,7 @@ jest.mock("../providers/CepProvider", () => {
             ceps: jest.fn(),
             handleSearch: jest.fn() 
         })
+    
     }
 })
 
@@ -46,17 +49,25 @@ describe("when everything is ok", () => {
 
     }) 
     test("when a available CEP is typed and the user click the button, will apear 6  new input's text ", async() => {
-        apiMock.onGet("/21820170").replyOnce(200,[{}])
-        render(<Search />)
-        render( <Adress />) 
+        apiMock.onGet("21820170").replyOnce(200, {"bairro": "Bangu", "cidade": "Rio de Janeiro", "logradouro": "Rua dos Tintureiros", "estado_info": {"area_km2": "43.781,566", "codigo_ibge": "33", "nome": "Rio de Janeiro"}, "cep": "21820170", "cidade_info": {"area_km2": "1200,179", "codigo_ibge": "3304557"}, "estado": "RJ"})
+        render(
+            <Providers>
+              <App />
+            </Providers>
+          );
         const input =  screen.getByPlaceholderText("Insira o CEP") 
         const button = screen.getByRole("button")
-        const inputSearch = screen.queryByTestId("inputSearch") 
-        
-        userEvent.type(input, "21820170") 
+        const form = await screen.queryByTestId("inputSearch") 
+
+        userEvent.type(input, "21820170")   
         userEvent.click(button) 
 
-       expect(inputSearch).toBeInTheDocument()
+        await waitFor(() => {
+            expect(input).toHaveValue(21820170)    
+                 
+            expect(screen.getByDisplayValue('Rua dos Tintureiros')).toBeInTheDocument() 
+        })
+
        
 
     })   
